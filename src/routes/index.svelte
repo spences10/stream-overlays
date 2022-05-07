ad4e3edf74653af5e66f2813a0dfd02a9c058b2f
spences10 <spences10apps@gmail.com>
2022-05-07 21:14:41 +0100

    feat: :sparkles: add generic embed for social stream
    add sessionId URL param

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { GenericEmbed } from 'sveltekit-embed';
 
 	export let title: string = $page.url.searchParams.get('title')?.toString() || 'Stream Overlay';
 	export let details: string =
 		$page.url.searchParams.get('details')?.toString() || 'Stream Overlay Details';
+	export let sessionId: string =
+		$page.url.searchParams.get('sessionId')?.toString() || 'BRZQF8gbvd';
 </script>
 
-<section class="absolute bottom-0 p-10 border-2 border-primary w-3/5 rounded-xl mb-8 ml-8">
+<section
+	class="absolute bottom-0 right-0 border-2 border-primary w-4/12 rounded-xl mb-8 mr-10 overflow-hidden p-4 h-52"
+>
+	<GenericEmbed
+		width="100%"
+		height="175px"
+		data-testid="spotify"
+		title="social-stream-chat-overlay"
+		src={`https://social-stream.vercel.app/dock.html?session=${sessionId}&hidesource&noavatar&notime&compact`}
+		frameBorder="0"
+		allow="encrypted-media"
+		overflow="hidden"
+		scrolling="no"
+	/>
+</section>
+
+<section class="absolute bottom-0 p-10 border-2 border-primary w-3/5 rounded-xl mb-8 ml-10 h-52">
 	<h1 class="text-5xl leading-relaxed tracking-wide text-shadow shadow-secondary">{title}</h1>
 	<p class="text-3xl leading-relaxed tracking-wide text-shadow shadow-secondary">{details}</p>
 </section>
