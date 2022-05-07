e4e08db9c09912c93775a844668062a3138ad79e
spences10 <spences10apps@gmail.com>
2022-05-07 15:39:37 +0100

    feat: :sparkles: init

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let title: string = $page.url.searchParams.get('title')?.toString() || 'Stream Overlay';
+	export let details: string =
+		$page.url.searchParams.get('details')?.toString() || 'Stream Overlay Details';
+</script>
+
+<section class="absolute bottom-0 p-10 border-2 border-primary w-3/5 rounded-xl mb-8 ml-8">
+	<h1 class="text-5xl leading-relaxed tracking-wide text-shadow shadow-secondary">{title}</h1>
+	<p class="text-3xl leading-relaxed tracking-wide text-shadow shadow-secondary">{details}</p>
+</section>
---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+const daisyui = require('daisyui');
+const plugin = require('tailwindcss/plugin');
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)'
+			}
+		}
+	},
+
+	plugins: [
+		daisyui,
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value
+					})
+				},
+				{ values: theme('textShadow') }
+			);
+		})
+	],
+
+	daisyui: {
+		themes: ['night']
+	}
+};
+
+module.exports = config;
